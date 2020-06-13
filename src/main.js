78fb479483feca9eb72567ace8f3b5b40dc0b097
Evgeniy Zavalishin <ezavalishin@gmail.com>
2020-06-13 23:48:07 +0400

    [REF] add markdown support

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VuePrism from 'vue-prism'
+import VueSimpleMarkdown from 'vue-simple-markdown'
 
 import 'prismjs/themes/prism-tomorrow.css'
+import 'vue-simple-markdown/dist/vue-simple-markdown.css'
 
 Vue.use(VuePrism)
+Vue.use(VueSimpleMarkdown)
 
 Vue.config.productionTip = false
 
